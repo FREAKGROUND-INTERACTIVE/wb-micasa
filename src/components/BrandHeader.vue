d9c3b4b925fb30264fe331b9906f13726029f7a6
Juan Castaneda <juavauj@gmail.com>
2021-02-05 13:38:31 -0500

    BRAND-HEADER COMPONENT Created

---
+++ src/components/BrandHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="brandheader">
+    <router-link :to="link"><img width="60" height="60" src="https://picsum.photos/1920/1080" alt="BrandHeader"></router-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      link: String
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+@import "./../assets/styles/setup";
+
+.brandheader {
+}
+</style>
