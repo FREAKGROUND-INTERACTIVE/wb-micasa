230db7d84dba72eb44f22386595b528c7fc5bf28
Juan Castaneda <juavauj@gmail.com>
2021-02-05 13:40:25 -0500

    STUDIO VIEW Updated

--- src/views/Studio.vue
+++ src/views/Studio.vue
@@ -1,15 +1,44 @@
 <template>
-    <div>
-        Studio
+  <transition>
+    <div class="studio">
+      <div class="studio__link-button">
+        <!-- <Link-button :link="link"></Link-button> -->
+      </div>
     </div>
+  </transition>
 </template>
 
 <script>
-    export default {
-        
-    }
+// import LinkButton from "@/components/LinkButton";
+
+export default {
+  name: "Studio",
+  components: {
+    // LinkButton,
+  },
+  data() {
+    return {
+      link: "/studio-02",
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
+@import "./../assets/styles/setup";
 
+.studio {
+  width: 100vw;
+  height: 100vh;
+  display: grid;
+  grid-template-columns: 1fr 0.5fr 2fr 1fr;
+  grid-template-rows: 1fr 5fr 1fr;
+
+  .studio__link-button {
+      grid-area: 3 / 4;
+      display: flex;
+      justify-content: center;
+      align-content: center;
+  }
+}
 </style>
