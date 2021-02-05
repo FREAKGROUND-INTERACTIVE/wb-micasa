98be497be207607e53122d5bf8993350902e4d38
NaN <ccmolinac@gmail.com>
2021-02-05 13:03:02 -0500

    AGENCY-ABOUT VIEW Update

---
+++ src/views/AgencyAbout.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="agency-about">
+    agency about
+    <Img-bg ref="imgViewTitle"></Img-bg>
+  </div>
+</template>
+
+<script>
+import {  mutations } from "./../state";
+import ImgBg from "@/components/ImgBg";
+export default {
+  components: {
+    ImgBg,
+  },
+  mounted() {
+    setTimeout(() => {
+      mutations.setTitle(" ");
+    }, 1000);
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
