9ff4ac2f88b6f9f8e6c2c10dedc99893fef240ad
NaN <ccmolinac@gmail.com>
2021-02-05 12:59:13 -0500

    STATE Update

--- src/state.js
+++ src/state.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 
-export const state = Vue.observable({ title: 'micasa' })
+export const state = Vue.observable({ title: '' })
 
 export const mutations = {
     setTitle(newVal) {
-        // console.log('Setting "title": ', newVal)
+        console.log('Setting "title": ', newVal)
         state.title = newVal
     }
 }
