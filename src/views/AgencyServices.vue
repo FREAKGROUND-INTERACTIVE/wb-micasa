3d655e68de0c9c5c5e889d5c9db6e9fbfab594bd
NaN <ccmolinac@gmail.com>
2021-02-08 15:18:20 -0500

    AGENCY-SERVICES VIEW Update

---
+++ src/views/AgencyServices.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="agency-services">
+    <div class="agency-services__button">
+      <Button
+        :mountedAnim="true"
+        :mountedDelay="1"
+        :text="'See our Services'"
+      ></Button>
+    </div>
+  </div>
+</template>
+
+<script>
+import Button from "@/components/Button";
+export default {
+  components: {
+    Button,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "./../assets/styles/setup";
+
+.agency-services {
+    @extend .layout;
+    .agency-services__button {
+        grid-area: content-2;
+        place-self: center;
+    }
+}
+</style>
