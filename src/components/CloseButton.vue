3a540c51d6ce9a28a975c0edaa53ad9bffdf20b5
Juan Castaneda <juavauj@gmail.com>
2021-02-08 15:17:42 -0500

    CLOSE-BUTTON COMPONENT Created

---
+++ src/components/CloseButton.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="close-button">
+    <div></div>
+    <div></div>
+  </div>
+</template>
+
+<script>
+import { gsap } from 'gsap';
+
+export default {
+  data() {
+    return {
+      lines: null
+    }
+  },
+  mounted() {
+    this.lines = this.$el.querySelectorAll(".close-button > div");
+    this.lines = [...this.lines];
+  },
+  methods: {
+    initAnim(delay) {
+      this.lines.forEach(element => {
+          gsap.to(element, {
+            duration: 1,
+            width: '100%',
+            delay: delay
+          });
+      });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "./../assets/styles/setup";
+
+.close-button {
+  width: 35px;
+  height: 35px;
+  cursor: pointer;
+  position: relative;
+
+  @include transform(rotate(0deg));
+  @include transition(all 0.5s);
+  @include transform-origin(center center);
+
+  div {
+    width: 0%;
+    height: 1px;
+    background-color: $dark;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+
+    &:first-child {
+    @include transform(translate(-50%, -50%) rotate(45deg));
+    }
+
+    &:last-child {
+    @include transform(translate(-50%, -50%) rotate(-45deg));
+    }
+  }
+
+  &:hover {
+    @include transform(rotate(180deg));
+  }
+}
+</style>
