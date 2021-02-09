c468a488693f9ff0e3ba5eea62c2208566edfdef
Juan Castaneda <juavauj@gmail.com>
2021-02-09 03:30:27 -0500

    SERVICES COMPONENT Updated
    - Adding initAnim
    - Adding initAnim mounted prop
    - Adding transition wrapper

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -1,18 +1,92 @@
 <template>
-  <div class="services">
-    <div class="services__title">
-      <h2>Our Services</h2>
+  <transition @leave="leave" :css="false">
+    <div class="services">
+      <div class="services__title">
+        <h2>Our Services</h2>
+      </div>
+      <div class="services__list">
+        <span v-for="service in list" :key="service.id">{{ service }}</span>
+      </div>
     </div>
-    <div class="services__list">
-      <span v-for="service in list" :key="service">{{ service }}</span>
-    </div>
-  </div>
+  </transition>
 </template>
 
 <script>
+import { gsap } from "gsap";
+
 export default {
   props: {
     list: Array,
+    mountedAnim: {
+      type: Boolean,
+      default: false,
+    },
+    mountedDelay: {
+      type: Number,
+      default: 0,
+    },
+  },
+  data() {
+    return {
+      listServices: null,
+      titleServices: null,
+    };
+  },
+  mounted() {
+    this.listServices = this.$el.querySelector(".services__list");
+    this.titleServices = this.$el.querySelector(".services__title");
+
+    //* initAnim function in mounted
+    if (this.mountedAnim) {
+      this.initAnim(this.mountedDelay);
+    }
+  },
+  methods: {
+    initAnim(delay) {
+      //* duration time for animation elements
+      let time = 1;
+
+      //* create timeLine
+      let initTl = gsap.timeline({
+        paused: "true",
+        delay: delay,
+      });
+
+      initTl.to(
+        this.titleServices,
+        {
+          duration: time,
+          opacity: 1,
+          ease: "power1.inOut",
+        },
+        "<0.2"
+      );
+
+      initTl.to(
+        this.listServices,
+        {
+          duration: time,
+          opacity: 1,
+          ease: "power1.inOut",
+        },
+        "<0.2"
+      );
+
+      
+      initTl.play();
+    },
+    /**
+     ** LEAVE FUNCTION
+     *? Function for leave behavior
+     * @param done it return the leave behavior end
+     */
+    leave(done) {
+      gsap.to(this.listServices, {
+        duration: 1.5,
+        opacity: 0,
+        onComplete: done,
+      });
+    },
   },
 };
 </script>
@@ -22,6 +96,7 @@ export default {
 
 .services {
   .services__title {
+    opacity: 0;
     h2 {
       text-align: center;
     }
@@ -32,6 +107,7 @@ export default {
     flex-flow: row wrap;
     justify-content: center;
     width: 30vw;
+    opacity: 0;
 
     span {
       &:after {
