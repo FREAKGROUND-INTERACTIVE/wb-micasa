9323203267120b1e0121859e4d512052afeba63b
Juan Castaneda <juavauj@gmail.com>
2021-02-09 03:25:39 -0500

    BRAND-HEADER COMPONENT Updated
    - Adding transition wrapper.
    - Adding initAnim
    - Adding initAnim mounted prop
    - Adding microinteractions
    - Adding leave

--- src/components/BrandHeader.vue
+++ src/components/BrandHeader.vue
@@ -1,20 +1,85 @@
 <template>
-  <div class="brandheader">
-    <router-link :to="link"><img width="60" height="60" src="https://picsum.photos/60/60" alt="BrandHeader"></router-link>
-  </div>
+  <transition @leave="leave" :css="false">
+    <div class="brandheader">
+      <router-link :to="link"
+        ><img
+          class="brandheader__img"
+          width="60"
+          height="60"
+          src="https://res.cloudinary.com/nancloud/image/upload/v1612852351/mi-casa/images/logo-test_xgyhou.svg"
+          alt="BrandHeader"
+      /></router-link>
+    </div>
+  </transition>
 </template>
 
 <script>
-  export default {
-    props: {
-      link: String
+import { gsap } from "gsap";
+
+export default {
+  props: {
+    link: String,
+    mountedAnim: {
+      type: Boolean,
+      default: false,
+    },
+    mountedDelay: {
+      type: Number,
+      default: 0,
+    },
+  },
+  data() {
+    return {
+      brandheader: null,
+    };
+  },
+  mounted() {
+    this.brandheader = this.$el.querySelector(".brandheader__img");
+
+    //* initAnim function in mounted
+    if (this.mountedAnim) {
+      this.initAnim(this.mountedDelay);
     }
-  }
+  },
+  methods: {
+    initAnim(delay) {
+      gsap.to(this.brandheader, {
+        duration: 2,
+        opacity: 1,
+        delay: delay,
+      });
+    },
+    /**
+     ** LEAVE FUCTION
+     *? Function for leave behavior
+     * @param done it return the leave behavior end
+     */
+    leave(done) {
+      gsap.to(this.brandheader, {
+        duration: 1.5,
+        opacity: 0,
+        onComplete: done,
+      });
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 @import "./../assets/styles/setup";
 
 .brandheader {
+  width: 60px;
+  height: 60px;
+  @include transition(all 0.5s);
+
+  .brandheader__img {
+    width: 100%;
+    opacity: 0;
+  }
+
+  &:hover {
+    @include transform(translateY(-10%));
+  }
 }
 </style>
