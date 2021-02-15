793597b0e3a4c9df7b48d0a8258ff0396f249ed0
NaN <ccmolinac@gmail.com>
2021-02-14 19:10:58 -0500

    STATE SERVICE Updated
    - Creating inHome observable
    - Creating setHome and getHGome methods

--- src/state.js
+++ src/state.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
 
-export const state = Vue.observable({ title: '' })
+export const state = Vue.observable({ title: '', inHome: false})
 
 export const mutations = {
     setTitle(newVal) {
         console.log('Setting "title": ', newVal)
         state.title = newVal
+    },
+    
+    setHome(newVal) {
+        console.log('Setting "inHome": ', newVal)
+        state.inHome = newVal
+    },
+
+    getHome() {
+        return state.inHome
     }
 }
