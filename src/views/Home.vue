8449d30ccb82b653e34c54c9bfb7f48830e2bb96
NaN <ccmolinac@gmail.com>
2021-02-14 19:11:41 -0500

    HOME VIEW Updated
    - Adding SVG colors to background

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <transition @leave="leave" :css="false">
     <div class="home">
-      <Home3d></Home3d>
+      <Color-layer ref="colorLayer" :color="color"></Color-layer>
+      <Home3d ref="home3d"></Home3d>
       <div class="home__links">
         <Links-list
           ref="linksList"
@@ -16,13 +17,46 @@
 <script>
 import { mutations } from "./../state";
 import { gsap } from "gsap";
+
+import ColorLayer from "@/components/ColorLayer";
 import Home3d from "@/components/Home3d";
 import LinksList from "@/components/LinksList";
+
 export default {
   name: "Home",
   components: {
+    ColorLayer,
     Home3d,
     LinksList,
+  },
+  props: {
+    color: String,
+  },
+  watch: {
+    color: function (val) {
+      setTimeout(() => {
+        switch (val) {
+          case "Agency":
+            this.$el.classList.remove("studio");
+            this.$el.classList.remove("powered");
+            this.$el.classList.add("agency");
+            break;
+          case "Studio":
+            this.$el.classList.remove("agency");
+            this.$el.classList.remove("powered");
+            this.$el.classList.add("studio");
+            break;
+          case "powered":
+            this.$el.classList.remove("agency");
+            this.$el.classList.remove("studio");
+            this.$el.classList.add("powered");
+            break;
+
+          default:
+            break;
+        }
+      }, 1000);
+    },
   },
   mounted() {
     gsap.to(this.$el, {
@@ -31,15 +65,22 @@ export default {
       delay: 0.5,
       onComplete: function () {
         mutations.setTitle("micasa");
+        mutations.setHome(true);
       },
     });
   },
   methods: {
     leave(el, done) {
+      this.$el.classList.remove("agency");
+      this.$el.classList.remove("studio");
+      this.$el.classList.remove("powered");
+      this.$refs.home3d.leave();
       this.$refs.linksList.leave();
+      this.$refs.colorLayer.leave();
+      mutations.setHome(false);
       gsap.to(el, {
         duration: 1,
-        opacity: "0",
+        y: 0,
         onComplete: done,
       });
     },
@@ -55,6 +96,7 @@ export default {
   opacity: 0;
   width: 100%;
   height: 100vh;
+  background-color: #dfdfdf;
 
   .home__links {
     position: absolute;
@@ -62,6 +104,18 @@ export default {
     bottom: 17%;
     left: 50%;
     @include transform(translateX(-50%));
+  }
+
+  &.agency {
+    background-color: #6a342e;
+  }
+
+  &.studio {
+    background-color: #2e6a30;
+  }
+
+  &.powered {
+    background-color: #2e496a;
   }
 }
 </style>
