b389d995b686bb17faa5254cd51db1a9418334e0
Juan Castaneda <juavauj@gmail.com>
2021-02-18 19:54:55 -0500

    LOADING COMPONENT Created

---
+++ src/components/Loading.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="loading">
+      <button @click="goToWeb">Continuar</button>
+    </div>
+</template>
+
+<script>
+import { gsap } from "gsap";
+export default {
+  methods: {
+    goToWeb() {
+      this.$emit("init");
+      gsap.to(this.$el, {
+        autoAlpha: 0
+      });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "./../assets/styles/setup";
+.loading {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: red;
+  z-index: 9999;
+  opacity: 1;
+}
+</style>
