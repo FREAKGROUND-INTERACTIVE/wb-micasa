f4b100f96c914a688bf6b03af57f3980c5be8dd0
Juan Castaneda <juavauj@gmail.com>
2021-03-03 13:43:07 -0500

    DATA ES Created
    - es.js file created for translation.

---
+++ src/assets/data/es.js
@@ -0,0 +1,17 @@
+export const es = {
+  titles: [
+    'agencia', 
+    'estudio',
+    'powered'
+  ],
+  agency: {
+    about: {
+      title: "Agencia",
+      paragraph: {
+        quote: "Acerca de",
+        title: "Mi Casa Studios",
+        text: "Aquí en Mi Casa Studios somos más\nque una agencia. Somos la base de operaciones\nde una comunidad creativa que apoya a\nsus miembros y genera un terreno fértil\npara que crezcan las ideas."
+      }
+    }
+  }
+}
