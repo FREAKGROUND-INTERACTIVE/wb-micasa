e248be2d518b7927fb946df4ca2ccc2aa01ccaad
Juan Castaneda <juavauj@gmail.com>
2021-03-03 13:50:28 -0500

    APP VUE Updated
    - Adding lang  for state.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <router-view :color="title" :initAudio="initAudio"/>
+    <router-view :color="title" :initAudio="initAudio" :data="dataLang"/>
     <View-title :text="title" :home="home"></View-title>
     <Menu></Menu>
 
-    <Loading @init="init" :count="loadingCount" :finish="loadingDone"></Loading>
+    <Loading @init="init" :count="loadingCount" :finish="loadingDone" v-if="false"></Loading>
     <!-- <Mouse :inHover="inHover"></Mouse> -->
   </div>
 </template>
 
 <script>
-import { state, mouse, loading, mutations } from "./state";
+import { state, mouse, loading, mutations, lang } from "./state";
 import ViewTitle from "@/components/ViewTitle";
 import Menu from "@/components/Menu";
 import Loading from "@/components/Loading";
@@ -38,6 +38,9 @@ export default {
     },
     loadingDone() {
       return loading.loaded;
+    },
+    dataLang() {
+      return lang.data;
     }
   },
   data() {
@@ -46,6 +49,7 @@ export default {
     }
   },
   mounted() {
+    console.log("data:", this.dataLang);
     let path = window.location.pathname;
     switch (path) {
       case "/Agency":
