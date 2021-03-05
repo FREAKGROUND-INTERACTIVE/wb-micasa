55ecf644e79eaf97259079ee7a6cb25a8b91edfb
Juan Castaneda <juavauj@gmail.com>
2021-03-04 23:38:12 -0500

    DATA ES Updated
    - All texts in Spanish.

--- src/assets/data/es.js
+++ src/assets/data/es.js
@@ -1,6 +1,6 @@
 export const es = {
   titles: [
-    'agencia', 
+    'agencia',
     'estudio',
     'powered'
   ],
@@ -12,6 +12,95 @@ export const es = {
         title: "Mi Casa Studios",
         text: "Aquí en Mi Casa Studios somos más\nque una agencia. Somos la base de operaciones\nde una comunidad creativa que apoya a\nsus miembros y genera un terreno fértil\npara que crezcan las ideas."
       }
+    },
+    altering: {
+      titleOne: "ALTERANDO",
+      titleTwo: "LO CONSTANTE",
+      paragraph: {
+        title: "Alterando lo Constante",
+        text: "Hacemos cosas valientes con el propósito de crear un cambio real\npara el Internet y más allá. En este mundo propulsado\npor la tecnología que parece moverse más rápido que la velocidad de la luz,\nsolo la narrativa correcta para las personas correctas en el momento correcto\nse abrirá paso."
+      }
+    },
+    services: {
+      titleOne: "UNA CASA",
+      titleTwo: "MUCHAS HABITACIONES",
+      button: "Vea nuestros servicios",
+      list: [
+        'Investigación <span>•</span>',
+        'Pronóstico de Tendencias <span>•</span>',
+        'Transmisión en Vivo',
+        'Desarrollo de PodCast <span>•</span>',
+        'Encuestas Cuantitativas <span>•</span>',
+        'Grupos de Enfoque',
+        'Innovación de Productos <span>•</span>',
+        'Identidad de Marca <span>•</span>',
+        'Márketing <span>•</span>',
+        'Publicidad',
+        'Desarrollo de Campañas <span>•</span>',
+        'Optimización <span>•</span>',
+        'Análisis de Datos <span>•</span>',
+        'Sitios Web',
+        'Estrategia y Creación de Contenido <span>•</span>',
+        'Redes Sociales Pagadas',
+        'Administración de la Comunidad <span>•</span>',
+        'Participación de Influencers',
+        'Diseño de Eventos <span>•</span>',
+        'Pop-Ups <span>•</span>',
+        'Diseño de Espacios',
+        'Soporte Para Ferias Comerciales'
+      ]
+    },
+    why: {
+      textPainter: "(Escoje un colo para pintar el objeto en el centro)",
+      title: "WHY US?",
+      paragraph: {
+        title: "¿Por qué nosotros?",
+        text: "En el corazón de nuestro proceso está la estrategia financiera. Con un\nprofundo conocimiento de nuestro consumidor Gen Z, sabemos cómo\nnuestro mensaje debe resonar para crear un cambio real\ndentro de un presupuesto."
+      }
+    },
+    clients: {
+      paragraph: {
+        title: "Nuestros Clientes",
+        text: "Somos apasionados por lo que creamos y atraemos a otros como nosotros. Los colaboradores ideales\nson agentes que buscan una nueva perspectiva. Juntos romperemos el status quo."
+      }
     }
+  },
+  locations: {
+    bro: {
+      infoStudioBrooklyn: {
+        title: "Mi Casa Brooklyn",
+        subtitle: "318 Grand Street, Suite 1G",
+        subtitle2: "Brooklyn, NY 11211",
+        text:
+          "Este luminoso estudio ofrece una experiencia única\ncon ventanas abatibles de acero del suelo al techo / pared a pared orientadas al oeste,\ntechos de 15 pies, ciclorama de 25 pies e interior de ladrillo a la vista.\nEl plano de planta abierta ofrece la máxima versatilidad con más de 3,000\npies cuadrados de espacio abierto convertible.",
+      },
+      infoHoodBrooklyn: {
+        title: "Williamsburg",
+        subtitle: "318 Grand Street, Suite 1G",
+        subtitle2: "Brooklyn, NY 11211",
+        text:
+          "Williamsburg es un barrio de moda en Brooklyn\nque atrae a los jóvenes y a la moda\na sus elegantes boutiques, cafés de moda y restaurantes populares.",
+      },
+    },
+    les: {
+      infoStudioLes: {
+        title: "Mi Casa LES",
+        subtitle: "318 Grand Street, Suite 1G",
+        subtitle2: "Brooklyn, NY 11211",
+        text:
+          "Este estudio está en el corazón del Lower East Side\nen un edificio que tiene una rica historia, en comunidad y arte\ne incluido en un documental del festival de cine de Tribeca. El plano\nde planta ofrece 1,800 pies cuadrados de espacio abierto convertible\ny un ciclorama de 20 pies.",
+      },
+      infoHoodLes: {
+        title: "Flacosburg",
+        subtitle: "318 Grand Street, Suite 1G",
+        subtitle2: "Brooklyn, NY 11211",
+        text:
+          "El ecléctico Lower East Side es donde los callejones y\nlos edificios de estilo residencial y la noche atraen a multitudes jóvenes\ny de moda a los bares, locales de música y\nrestaurantes de moda de la zona.",
+      }
+    },
+    buttonN: "El vecindario"
+  },
+  powered: {
+    title: "Desarrollado por Mi Casa Studios"
   }
 }
