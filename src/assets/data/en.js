af03f11094db24a63a8950af8d94520d21aa0432
Juan Castaneda <juavauj@gmail.com>
2021-03-04 23:37:45 -0500

    DATA EN Updated
    - All texts in english.

--- src/assets/data/en.js
+++ src/assets/data/en.js
@@ -1,6 +1,6 @@
 export const en = {
   titles: [
-    'agency', 
+    'agency',
     'studio',
     'powered'
   ],
@@ -12,6 +12,95 @@ export const en = {
         title: "Mi Casa Studios",
         text: "Here at Mi Casa Studios we are more\nthan an agency. We are the home base\nfor a creative community that nurtures\nits menbers and provides fertile\nground for ideas to grow."
       }
+    },
+    altering: {
+      titleOne: "ALTERING",
+      titleTwo: "THE CONSTANT",
+      paragraph: {
+        title: "Altering the constant",
+        text: "We make bold things with a purpose to create real change\nfor the internet and beyond that. In this technology driven\nworld that seems to move faster than the speed of light,\nonly the right narrative to the right people at the right time\nwill pierce through."
+      }
+    },
+    services: {
+      titleOne: "ONE HOME",
+      titleTwo: "MANY ROOMS",
+      button: "See our Services",
+      list: [
+        'Research <span>•</span>',
+        'Trend forecasting <span>•</span>',
+        'Live Streaming <span>•</span>',
+        'PodCast Development',
+        'Quantitative Surveys <span>•</span>',
+        'Focus Groups <span>•</span>',
+        'Product Innovation <span>•</span>',
+        'Brand Identity',
+        'Marketing Advertising <span>•</span>',
+        'Campaign Development <span>•</span>',
+        'Optimization',
+        'Data Analysis <span>•</span>',
+        'Websites <span>•</span>',
+        'Content Strategy & Creation <span>•</span>',
+        'Paid Social',
+        'Community Management <span>•</span>',
+        'Influencer Engagement <span>•</span>',
+        'Event Design',
+        'Pop-Ups <span>•</span>',
+        'Space Design <span>•</span>',
+        'Tradeshow Support <span>•</span>',
+        'Whatever It Takes',
+      ]
+    },
+    why: {
+      textPainter: "(Escoje un colo para pintar el objeto en el centro)",
+      title: "WHY US?",
+      paragraph: {
+        title: "Why us?",
+        text: "At the heart of our process is financial strategy. With a\ndeep understanding of our Gen Z consumer, we know how\nour message needs to resonate to create real change\nwithin a budget."
+      }
+    },
+    clients: {
+      paragraph: {
+        title: "Our Clients",
+        text: "We are passionate about what we create and attract others like us. Ideal collaborators\nare agents who seek a fresh perspective. Together we disrupt the status quo"
+      }
     }
+  },
+  locations: {
+    bro: {
+      infoStudioBrooklyn: {
+        title: "Mi Casa Brooklyn",
+        subtitle: "318 Grand Street, Suite 1G",
+        subtitle2: "Brooklyn, NY 11211",
+        text:
+          "This bright studio offers a one-of-a-kind experience with west\nfacing floor-to-ceiling/wall-to-wall steel casement windows,\nfifteen feet ceilings, 25 foot cyclorama, and exposed brick interior.\nThe open floor plan offers maximum versatility with over 3,000\n square feet of open convertible space.",
+      },
+      infoHoodBrooklyn: {
+        title: "Williamsburg",
+        subtitle: "318 Grand Street, Suite 1G",
+        subtitle2: "Brooklyn, NY 11211",
+        text:
+          "Williamsburg is a hip neighborhood in Brooklyn\nthat draws the young and the fashionable to its\nchic boutiques, trendy cafes and buzzy restaurants.",
+      },
+    },
+    les: {
+      infoStudioLes: {
+        title: "Mi Casa LES",
+        subtitle: "318 Grand Street, Suite 1G",
+        subtitle2: "Brooklyn, NY 11211",
+        text:
+          "This studio is in the heart of the Lower East Side in a\nbuilding that has a rich history, in community and art and\nonce in a Tribeca film festival documentary. The floor \noffers 1,800 square feet of open convertible space and a\n20 foot cyclorama.",
+      },
+      infoHoodLes: {
+        title: "Flacosburg",
+        subtitle: "318 Grand Street, Suite 1G",
+        subtitle2: "Brooklyn, NY 11211",
+        text:
+          "The eclectic Lower East Side is where gritty alleys and\ntenement-style buildings and nighttime draws hip, young\ncrowds to the area's trendy bars, music venues and\nrestaurants.",
+      },
+    },
+    buttonN: "Neighborhood"
+  },
+  powered: {
+    title: "Powered by Mi Casa Studios"
   }
 }
